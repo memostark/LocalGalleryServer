ab457754c06fcbcecd4db180247df17d73ba389a
Memo <memito_095@hotmail.com>
2020-03-24 14:28:43 -0600

    Initial commit.

---
+++ src/main/kotlin/com/guillermonegrete/gallery/FoldersController.kt
@@ -0,0 +1,13 @@
+package com.guillermonegrete.gallery
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FoldersController{
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) = "Hello, $name"
+    
+}
---
+++ src/main/kotlin/com/guillermonegrete/gallery/GalleryApplication.kt
@@ -0,0 +1,11 @@
+package com.guillermonegrete.gallery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GalleryApplication
+    
+fun main(args: Array<String>) {
+    runApplication<GalleryApplication>(*args)
+}
