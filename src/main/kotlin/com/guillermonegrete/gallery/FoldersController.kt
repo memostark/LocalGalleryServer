94eaac0c13cc31bfb62476e09dcb18bec93f309f
Memo <memito_095@hotmail.com>
2020-03-24 22:43:57 -0600

    Returns full cover url of each folder.

--- src/main/kotlin/com/guillermonegrete/gallery/FoldersController.kt
+++ src/main/kotlin/com/guillermonegrete/gallery/FoldersController.kt
@@ -6,19 +6,22 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.io.File
+import java.net.InetAddress
 
 @RestController
 class FoldersController(val repository: FoldersRepository){
 
     @Value("\${BASE_PATH}")
     private lateinit var basePath: String
+    private val ipAddress: String by lazy { getLocalIpAddress() }
 
     @GetMapping("/folders")
     fun rootFolders(): GetFolderResponse{
 
         val folders = repository.getFolders(basePath).map {
             val folder = repository.getFolders("$basePath/$it")
-            val coverUrl = getFirstImageFile(folder)
+            val coverFilename = getFirstImageFile(folder)
+            val coverUrl = "http://$ipAddress/images/$it/$coverFilename"
 
             Folder(it, coverUrl, folder.size)
         }
@@ -27,10 +30,19 @@ class FoldersController(val repository: FoldersRepository){
     }
 
 
+    /**
+     * Gets the first filename that is a image of the given list.
+     */
     fun getFirstImageFile(files: List<String>): String{
         return files.firstOrNull {
             image_suffixes.contains( it.split(".").last() )
         } ?: ""
+    }
+
+
+    fun getLocalIpAddress(): String{
+        val inetAddress = InetAddress.getLocalHost()
+        return inetAddress.hostAddress
     }
 
     companion object{
