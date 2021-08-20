9d70013b8d0b92ffe1af4d6e6d524e0102c94881
Memo <memito_095@hotmail.com>
2021-08-19 21:02:31 -0500

    MediaFile's filename is now unique, calculate the media file values at the start of the execution.

--- src/main/kotlin/com/guillermonegrete/gallery/data/MediaFile.kt
+++ src/main/kotlin/com/guillermonegrete/gallery/data/MediaFile.kt
@@ -1,17 +1,14 @@
 package com.guillermonegrete.gallery.data
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 
 @Entity
 data class MediaFile(
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    val id: Long = 0,
+    @Column(unique=true)
     val filename: String,
     val width: Int,
-    val height: Int
+    val height: Int,
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = 0
 )
