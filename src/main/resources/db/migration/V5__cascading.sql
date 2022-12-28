95d499f79d4c9ce34ae267eb836d70bf1387902d
Memo <memito_095@hotmail.com>
2022-12-27 21:51:13 -0600

    Adding on delete cascading to the constraints in the file/tag join table. This allows to delete a file/tag, the other tag/file of the relationship is not deleted.

--- src/main/resources/db/migration/V5__cascading.sql
+++ src/main/resources/db/migration/V5__cascading.sql
@@ -2,3 +2,13 @@ ALTER TABLE media_file
 DROP FOREIGN KEY FK_MEDIAFILE_ON_FOLDER;
 ALTER TABLE media_file
 ADD CONSTRAINT FK_MEDIAFILE_ON_FOLDER FOREIGN KEY (folder_id) REFERENCES media_folder (id) ON DELETE CASCADE;
+
+ALTER TABLE media_tags
+DROP FOREIGN KEY constr_mediatags_media_fk;
+ALTER TABLE media_tags
+ADD CONSTRAINT constr_mediatags_media_fk FOREIGN KEY (media_id) REFERENCES media_file (id) ON DELETE CASCADE;
+
+ALTER TABLE media_tags
+DROP FOREIGN KEY constr_mediatags_tag_fk;
+ALTER TABLE media_tags
+ADD CONSTRAINT constr_mediatags_tag_fk FOREIGN KEY (tag_id) REFERENCES tag_entity (id) ON DELETE CASCADE;
