8f1ce9f0d4ceb49306978819d6d204d8689fac63
Memo <memito_095@hotmail.com>
2023-02-07 21:54:24 -0600

    Added support for reading dimensions of WebP VP8 (Simple File Format).

--- src/main/kotlin/com/guillermonegrete/gallery/services/GetFileInfoService.kt
+++ src/main/kotlin/com/guillermonegrete/gallery/services/GetFileInfoService.kt
@@ -10,6 +10,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.attribute.BasicFileAttributes
 import java.time.Instant
+import java.util.*
 import javax.imageio.ImageIO
 import javax.imageio.stream.FileImageInputStream
 
@@ -69,11 +70,31 @@ class GetFileInfoService {
      * Support only for WebP Extended File Format (VP8X)
      * More information: https://stackoverflow.com/questions/64548364/parsing-webp-file-header-in-kotlin-to-get-its-height-and-width-but-getting-unex
      */
-    private fun getWebPDimensions(imgFile: File): Size {
+    private fun getWebPDimensions(imgFile: File): Size? {
         val stream = FileInputStream(imgFile)
         val data = stream.readNBytes(30)
-        val width = 1 + (get24bit(data, 24))
-        val height = 1 + (get24bit(data, 27))
+        // All formats consist of a file header (12 bytes) and a ChunkHeader (8 bytes)
+        // The first four ChunkHeader bytes contain the 4 characters of the format (12 to 15):
+        val imageFormat = String(Arrays.copyOfRange(data, 12, 16)) // exclusive range
+        val width: Int
+        val height: Int
+        when(imageFormat) {
+            "VP8 " -> { // last character is a space
+                // Simple File Format (Lossy)
+                // The data is in the VP8 specification and the decoding guide explains how to get the dimensions: https://datatracker.ietf.org/doc/html/rfc6386#section-19.1
+                // The formats consists of the frame_tag (3 bytes), start code (3 bytes), horizontal_size_code (2 bytes) and vertical_size_code (2 bytes)
+                // The size is 12 bits, use a mask to remove the last two digits
+                width = get16bit(data, 26) and 0x3FFF
+                height = get16bit(data, 28) and 0x3FFF
+            }
+            "VP8X" -> {
+                // Extended File Format, size position specified here: https://developers.google.com/speed/webp/docs/riff_container#extended_file_format
+                // The width starts 4 bytes after the ChunkHeader with a size of 3 bytes, the height comes after.
+                width = 1 + (get24bit(data, 24))
+                height = 1 + (get24bit(data, 27))
+            }
+            else -> return null
+        }
         return Size(width, height)
     }
 
@@ -95,8 +116,12 @@ class GetFileInfoService {
         }
     }
 
+    private fun get16bit(data: ByteArray, index: Int): Int {
+        return data[index].toInt() and 0xFF or (data[index + 1].toInt() and 0xFF shl 8)
+    }
+
     private fun get24bit(data: ByteArray, index: Int): Int {
-        return data[index].toInt() and 0xFF or (data[index + 1].toInt() and 0xFF shl 8) or (data[index + 2].toInt() and 0xFF shl 16)
+        return get16bit(data, index) or (data[index + 2].toInt() and 0xFF shl 16)
     }
 
     data class Size(val width: Int, val height: Int)
