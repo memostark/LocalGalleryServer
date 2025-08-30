1c966ecfcd3ea30f376a20bdc82e03d8ed73ae5c
Memo <memito_095@hotmail.com>
2025-08-29 23:59:23 -0600

    When sorting by count, the page items now include the count. This avoids recalculating.

--- src/main/kotlin/com/guillermonegrete/gallery/repository/MediaFolderRepository.kt
+++ src/main/kotlin/com/guillermonegrete/gallery/repository/MediaFolderRepository.kt
@@ -18,17 +18,21 @@ interface MediaFolderRepository: JpaRepository<MediaFolder, Long>{
     /**
      * Returns all media folders by the count of their children files.
      */
-    @Query(value = "SELECT * FROM media_folder " +
-            "group by media_folder.id order by (SELECT count(folder_id) FROM media_file where folder_id = media_folder.id) asc, media_folder.id",
+    @Query(value = "SELECT name, " +
+            "IFNULL((SELECT filename FROM media_file where media_file.id = cover_file_id), (SELECT filename FROM media_file where media_file.folder_id = media_folder.id LIMIT 1)) as coverUrl, " + //
+            "(SELECT count(folder_id) FROM media_file where folder_id = media_folder.id) as count, id FROM media_folder " +
+            "group by media_folder.id order by count asc, media_folder.id",
         countQuery = "SELECT count(*) FROM media_folder",
         nativeQuery = true)
-    fun findAllMediaFolderByFileCountAsc(pageable: Pageable): Page<MediaFolder>
+    fun findAllMediaFolderByFileCountAsc(pageable: Pageable): Page<FolderDto>
 
-    @Query(value = "SELECT * FROM media_folder " +
-            "group by media_folder.id order by (SELECT count(folder_id) FROM media_file where folder_id = media_folder.id) desc, media_folder.id",
+    @Query(value = "SELECT name, " +
+            "IFNULL((SELECT filename FROM media_file where media_file.id = cover_file_id), (SELECT filename FROM media_file where media_file.folder_id = media_folder.id LIMIT 1)) as coverUrl, " + //
+            "(SELECT count(folder_id) FROM media_file where folder_id = media_folder.id) as count, id FROM media_folder " +
+            "group by media_folder.id order by count desc, media_folder.id",
         countQuery = "SELECT count(*) FROM media_folder",
         nativeQuery = true)
-    fun findAllMediaFolderByFileCountDesc(pageable: Pageable): Page<MediaFolder>
+    fun findAllMediaFolderByFileCountDesc(pageable: Pageable): Page<FolderDto>
 
     /**
      * Query is the same as the regular one with an added WHERE clause that returns only the folders containing the query.
@@ -44,4 +48,11 @@ interface MediaFolderRepository: JpaRepository<MediaFolder, Long>{
         countQuery = "select count(f) from MediaFolder f"
     )
     fun findByNameContainingAndFileCountDesc(name: String, pageable: Pageable): Page<MediaFolder>
+}
+
+interface FolderDto {
+    val name:String
+    val coverUrl: String?
+    val count: Int
+    val id: Long
 }
