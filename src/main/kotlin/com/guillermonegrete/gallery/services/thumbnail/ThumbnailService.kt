e4d9d1a36893cab92e0d7f9720c37677c7ab671f
Memo <memito_095@hotmail.com>
2025-11-09 00:25:08 -0600

    Thumbnails for videos are now generated. If the thumbnail already exists then the service returns its byte array.

--- src/main/kotlin/com/guillermonegrete/gallery/services/thumbnail/ThumbnailService.kt
+++ src/main/kotlin/com/guillermonegrete/gallery/services/thumbnail/ThumbnailService.kt
@@ -1,5 +1,7 @@
 package com.guillermonegrete.gallery.services.thumbnail
 
+import net.bramp.ffmpeg.FFmpegExecutor
+import net.bramp.ffmpeg.builder.FFmpegBuilder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.awt.image.BufferedImage
@@ -8,28 +10,51 @@ import java.io.File
 import javax.imageio.ImageIO
 
 @Service
-class ThumbnailService(@Value("\${final.path}") private val basePath: String) {
+class ThumbnailService(@param:Value("\${final.path}") private val basePath: String) {
 
     fun generateThumbnail(folder: String, filename: String, type: ThumbnailType): ByteArray {
 
-        // Generate scaled image
         val baseFolder = File(basePath, folder)
         val originalFile = File(baseFolder, filename)
-        val originalImage = ImageIO.read(originalFile)
-        val newHeight = (type.size * (originalImage.height.toFloat() / originalImage.width)).toInt()
-        val buffImg = BufferedImage(type.size, newHeight, BufferedImage.TYPE_INT_RGB)
-        buffImg.createGraphics().drawImage(originalImage.getScaledInstance(type.size, newHeight, BufferedImage.SCALE_SMOOTH), 0, 0, null)
 
-        // Write to file
+        // Check if thumbnail already exists
         val thumbnailsFolder = File(baseFolder, THUMBNAILS_FOLDER)
         if (!thumbnailsFolder.exists()) thumbnailsFolder.mkdir()
-        val newFilename = originalFile.nameWithoutExtension + "-" + type.name.lowercase() + "." + originalFile.extension
+        val newFilename = originalFile.nameWithoutExtension + "-" + type.name.lowercase() + "." + THUMBNAIL_EXT
         val newFile = File(thumbnailsFolder, newFilename)
-        ImageIO.write(buffImg, THUMBNAIL_EXT, newFile)
+        if (newFile.exists()) {
+            val thumbnail = ImageIO.read(newFile)
+            val baos = ByteArrayOutputStream()
+            ImageIO.write(thumbnail, THUMBNAIL_EXT, baos)
+            return baos.toByteArray()
+        }
+
+        // Otherwise generate new thumbnail
+        val originalImage = ImageIO.read(originalFile)
+        val img = if (originalImage != null) {
+            val newHeight = (type.size * (originalImage.height.toFloat() / originalImage.width)).toInt()
+            val buffImg = BufferedImage(type.size, newHeight, BufferedImage.TYPE_INT_RGB)
+            buffImg.createGraphics().drawImage(originalImage.getScaledInstance(type.size, newHeight, BufferedImage.SCALE_SMOOTH), 0, 0, null)
+
+            ImageIO.write(buffImg, THUMBNAIL_EXT, newFile)
+            buffImg
+        } else {
+            val builder = FFmpegBuilder()
+                .setInput(originalFile.absolutePath)
+                .addOutput(newFile.absolutePath)
+                .setVideoCodec("libwebp")
+                .setFrames(1)
+                .setVideoFilter("scale=${type.size}:-1")
+                .done()
+            val executor = FFmpegExecutor()
+            executor.createJob(builder).run()
+
+            ImageIO.read(newFile)
+        }
 
         // Return byte data
         val baos = ByteArrayOutputStream()
-        ImageIO.write(buffImg, THUMBNAIL_EXT, baos)
+        ImageIO.write(img, THUMBNAIL_EXT, baos)
         return baos.toByteArray()
     }
 
@@ -37,10 +62,15 @@ class ThumbnailService(@Value("\${final.path}") private val basePath: String) {
         const val THUMBNAILS_FOLDER = ".thumbnails"
         const val THUMBNAIL_EXT = "webp"
     }
+}
 
-    enum class ThumbnailType(val size: Int) {
-        Small(100),
-        Medium(200),
-        Large(300)
+enum class ThumbnailType(val size: Int) {
+    Small(100),
+    Medium(200),
+    Large(300);
+
+    companion object {
+        fun getThumbnailType(size: String) =
+            ThumbnailType.entries.find { it.name.compareTo(size, true) == 0 }
     }
 }
