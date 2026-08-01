7b2fdb939bc1f71db1a31cd95048ef09b62e68c2
Memo <memito_095@hotmail.com>
2026-07-31 23:33:28 -0600

    Upgraded to Kotlin 2.4.10. (#115)
    Also, updated Gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 	id("org.springframework.boot") version "3.5.7"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	id ("org.flywaydb.flyway") version "7.14.0"
-	val kotlinVersion = "2.2.0"
+	val kotlinVersion = "2.4.10"
 	kotlin("jvm") version kotlinVersion
 	kotlin("plugin.spring") version kotlinVersion
 }
